c8f08fc6e1f5dd72b10a7fff96168e16dceec2f0
Kaung Si Thu <kaungsithugalaxynote10plus@gmail.com>
2025-02-24 19:54:43 +0700

    done

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -48,10 +48,9 @@
 
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.0.0/dist/flowbite.min.js"></script>
 </body>
-<div id="profile-modal" tabindex="-1" aria-hidden="true" 
-class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-<div class="relative p-4 w-full max-w-4xl max-h-full">
-    <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+<div id="profile-modal" tabindex="-1" aria-hidden="true" class="hidden fixed inset-0 flex justify-center items-center z-50 bg-black bg-opacity-50">
+<div class="p-4 w-full max-w-4xl max-h-full">
+    <div class="bg-white rounded-lg shadow-sm dark:bg-gray-700">
         <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
             <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
                 Admin Profile
@@ -71,7 +70,6 @@ class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 
             <form id="update-admin-form" method="POST" action="{{ route('admin.update.profile') }}" class="space-y-4">
                 @csrf
                 @method('PUT')
-
                 <div>
                     <label for="profile-id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Admin ID</label>
                     <input type="text" name="admin_id" id="profile-id" readonly 
